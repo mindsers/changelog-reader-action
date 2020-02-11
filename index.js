9e6a9bbd291b927c6ccc928ede4b2b22a3e420b6
Mindsers <dev@nathanaelcherrier.com>
2020-02-12 00:44:03 +0100

    Parse the a changelog

--- index.js
+++ index.js
@@ -1,21 +1,48 @@
-const core = require('@actions/core');
-const wait = require('./wait');
+const core = require('@actions/core')
+const fs = require('fs')
 
 
 // most @actions toolkit packages have async methods
 async function run() {
-  try { 
-    const ms = core.getInput('milliseconds');
-    console.log(`Waiting ${ms} milliseconds ...`)
+  try {
+    const changelogPath = core.getInput('path') || './CHANGELOG.md'
+    const targetVersion = core.getInput('version')
 
-    core.debug((new Date()).toTimeString())
-    wait(parseInt(ms));
-    core.debug((new Date()).toTimeString())
+    fs.readFile(changelogPath, (err, data) => {
+      if (err) {
+        core.setFailed(err.message)
+        return
+      }
 
-    core.setOutput('time', new Date().toTimeString());
-  } 
+      const versions = String(data)
+        .split('\n## ')
+        .filter(version => /^\[v/.test(version))
+        .map(version => {
+          const [title, ...other] = version.split('\n')
+          const [versionNumber, versionDate] = title.replace(/(\[|\])/g, '').split(' - ')
+          return {
+            version: versionNumber,
+            date: versionDate,
+            log: other
+              .filter(item => !/\[.*\]: http/
+              .test(item)).join('\n')
+          }
+        })
+
+      if (targetVersion != null) {
+        const entry = versions.find(item => item.version === targetVersion)
+
+        if (entry != null) {
+          core.setOutput('logEntry', entry.log)
+          return
+        }
+      }
+
+      core.setOutput('logEntry', versions[0].log)
+    })
+  }
   catch (error) {
-    core.setFailed(error.message);
+    core.setFailed(error.message)
   }
 }
 
