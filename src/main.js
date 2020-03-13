4e2886826de7ab071a8f74006586dc4758aad6aa
Mindsers <dev@nathanaelcherrier.com>
2020-03-13 21:57:06 +0100

    Add some logs

--- src/main.js
+++ src/main.js
@@ -13,9 +13,17 @@ exports.main = async function main() {
     const changelogPath = core.getInput('path') || './CHANGELOG.md'
     const targetVersion = core.getInput('version')
 
+    if (targetVersion == null) {
+      core.warning(`No target version specified. try to return the most recent one in the changelog file.`)
+    }
+
+    core.startGroup('Parse data')
     const rawData = await readFile(changelogPath)
     const versions = getEntries(rawData)
       .map(parseEntry)
+
+    core.debug(`${version.length} version logs found`)
+    core.endGroup()
 
     const version = getVersionById(versions, targetVersion)
 
