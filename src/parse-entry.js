fc0d47b2e7c3d35fb67e3d0f5048b1e02ff61590
Nathanael Cherrier <dev@nathanaelcherrier.com>
2020-07-03 10:25:04 +0200

    First version of the parse function working with semver
    This version is too smart...

--- src/parse-entry.js
+++ src/parse-entry.js
@@ -1,12 +1,15 @@
+const capturingVersionAndDateRegex = /\[?(v?(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?|unreleased)\]?(\s+-\s+)?(.*)$/i
+
 exports.parseEntry = entry => {
   const [title, ...other] = entry
     .trim()
     .split('\n')
 
-  const [versionNumber, versionDate] = title.replace(/(\[|\])/g, '').split(' - ')
+  const [, versionNumber, ...lastCaptureGroup] = title.match(capturingVersionAndDateRegex)
+  const [versionDate] = lastCaptureGroup.reverse()
 
   return {
-    id: versionNumber.match(/(\w|\.)/g).join(''),
+    id: versionNumber.replace(/(\s)/g, ''),
     date: versionDate,
     text: other
       .filter(item => !/\[.*\]: http/.test(item))
