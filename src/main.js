d8c8d89f16a34cdce71f12e98b06f1153bc2c6cd
Nathanael CHERRIER <nathanael@cherrier.co>
2022-04-19 08:26:17 +0200

    Fix imports

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ const { validateEntry } = require('./validate-entry')
 const { parseEntry } = require('./parse-entry')
 const { getEntries } = require('./get-entries')
 const { getVersionById } = require('./get-version-by-id')
+const { getLinks } = require('./get-links')
 const { addLinks } = require('./add-links')
 
 const readFile = utils.promisify(fs.readFile)
